4b60d4fb24bad4dcb77bd66c50977330f74d3d95
Adrian Philipp <adrian@ticketswap.com>
2017-03-19 15:24:57 +0100

    Heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -15,11 +15,21 @@ use Mix.Config
 # which you typically run after static files are built.
 config :estimator, Estimator.Web.Endpoint,
   on_init: {Estimator.Web.Endpoint, :load_from_system_env, []},
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "blooming-woodland-23562.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+config :estimator, Estimator.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
 
 # ## SSL Support
 #
@@ -61,4 +71,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+#import_config "prod.secret.exs"
--- lib/estimator/web/channels/user_socket.ex
+++ lib/estimator/web/channels/user_socket.ex
@@ -5,7 +5,9 @@ defmodule Estimator.Web.UserSocket do
   channel "estimation:*", Estimator.Web.EstimationChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+      timeout: 45_000
+
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
