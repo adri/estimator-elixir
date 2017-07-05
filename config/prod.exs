8107902ac7468a59129dc6a4966f87a77815d112
Adrian Philipp <adrian@ticketswap.com>
2017-07-05 11:52:32 +0200

    Use dynamic host

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :estimator, Estimator.Web.Endpoint,
   on_init: {Estimator.Web.Endpoint, :load_from_system_env, []},
-  url: [scheme: "https", host: "blooming-woodland-eu-23562.herokuapp.com", port: 443],
+  url: [scheme: "https", host: System.get_env("HOST"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
