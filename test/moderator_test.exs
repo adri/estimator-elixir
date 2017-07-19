6bbf7db48863a66f4a0064d878eeefec48d76f87
Adrian Philipp <adrian@ticketswap.com>
2017-07-19 11:46:53 +0200

    Moderator test cleanup

--- test/moderator_test.exs
+++ test/moderator_test.exs
@@ -4,23 +4,23 @@ defmodule Estimator.ModeratorTest do
   alias Estimator.Moderator
 
   setup do
-    {:ok, moderator} = Moderator.start_link(name: {:global, __MODULE__})
-    {:ok, moderator: moderator}
+    {:ok, subject} = Moderator.start_link(name: {:global, __MODULE__})
+    {:ok, subject: subject}
   end
 
-  test ".set moderator for topic", %{moderator: moderator} do
-    Moderator.set_for_topic(1, 'topic', moderator)
+  test ".set moderator for topic", %{subject: subject} do
+    Moderator.set_for_topic(1, 'topic', subject)
   end
 
-  test ".get moderator for topic", %{moderator: moderator} do
-    Moderator.set_for_topic(1, 'topic', moderator)
+  test ".get moderator for topic", %{subject: subject} do
+    Moderator.set_for_topic(1, 'topic', subject)
 
-    assert Moderator.get_for_topic('topic', moderator) === 1
+    assert Moderator.get_for_topic('topic', subject) === 1
   end
 
-  test ".determine for topic", %{moderator: moderator} do
-    Moderator.set_for_topic(2, 'topic', moderator)
+  test ".determine for topic", %{subject: subject} do
+    Moderator.set_for_topic(2, 'topic', subject)
 
-    assert Moderator.determine_moderator([1, 2, 3], 'topic', moderator) === 2
+    assert Moderator.determine_moderator([1, 2, 3], 'topic', subject) === 2
   end
 end
